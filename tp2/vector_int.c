0e511341dcde50b67d60ce0fdf1baea793d35dfe
Luca Uriarte <lucahermanete@hotmail.com>
2024-04-05 15:13:45 -0300

    Add function to add two vectors

--- tp2/vector_int.c
+++ tp2/vector_int.c
@@ -35,9 +35,47 @@ VECTOR_ELEMENT vector_operate(vector* v, VECTOR_ELEMENT (*operation)(VECTOR_ELEM
     return res;
 }
 
+vector* vector_sum(vector* a, vector* b)
+{
+    int status = 0; 
+    int size = 0;
+    vector* result;
+    //Compruebo si los dos vectores tienen el mismo tamano
+    if(vector_size(a) != vector_size(b))
+    {
+        status = -1;
+    }
+
+    if(status == 0)
+    {
+        size = vector_size(a);
+        result = vector_new(size);
+
+        for(int i=0; i< size; i++)
+        {
+            vector_add(result, vector_get(a, i) + vector_get(b, i));
+        }
+    }
+    
+    return result;
+}
+
 int main()
 {
     vector* v = NULL;
+    vector* v1 = NULL;
+    vector* v2 = NULL;
+    vector* v3 = NULL;
+
+    v1 = vector_new(3);
+    v2 = vector_new(3);
+    vector_add(v1, 2);
+    vector_add(v1, 3);
+    vector_add(v1, 6);
+    vector_add(v2, 2);
+    vector_add(v2, 3);
+    vector_add(v2, 6);
+
     int sum = 0;
 
     v = vector_new(100);
@@ -46,6 +84,10 @@ int main()
 
     sum = vector_operate(v, add_int);
     printf("Suma de todos los elementos del vector: %i\n", sum);
+
+    v3 = vector_sum(v1, v2);
+
+    vector_print(v3, print_int);
 
     return 0;
 }
