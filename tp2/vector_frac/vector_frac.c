e96d9c544cb74f2d65d72d037bfca8750fbe521c
Luca Uriarte <lucahermanete@hotmail.com>
2024-04-07 17:10:52 -0300

    Add function to remove equivalent fractions

--- tp2/vector_frac/vector_frac.c
+++ tp2/vector_frac/vector_frac.c
@@ -13,6 +13,25 @@ void fill_100_random_frac(vector* v)
     }
 }
 
+void remove_equivalent_fracs(vector* v)
+{
+    int index = 0;
+    int size = vector_size(v);
+    while(index < size)
+    {
+        fraction* e = vector_get(v, index);
+        for(int i=(index+1); i<size; i++)
+        {
+            if(fraction_cmp(e, vector_get(v, i)) == 0)
+            {
+                vector_remove(v, i);
+                size--;
+            }
+        }
+        index++;
+    }
+}
+
 int main()
 {
     vector* v = NULL;
@@ -20,6 +39,7 @@ int main()
     v = vector_new(100);
     fill_100_random_frac(v);
     vector_print(v, fraction_print);
-
+    remove_equivalent_fracs(v);
+    vector_print(v, fraction_print);
     return 0;
 }
