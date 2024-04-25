36b2a5a0c2ed8143d353014d5dda22e8c82d01bc
Luca Uriarte <lucahermanete@hotmail.com>
2024-04-25 15:19:07 -0300

    Add sll and remove duplicated elements

---
+++ tp4/ssl.c
@@ -0,0 +1,125 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define t_elem int
+
+typedef struct _sll_node 
+{
+    t_elem data;
+    struct _sll_node* next;
+} sll_node;
+
+void print_int(int value)
+{
+    printf("%i ", value);
+}
+
+int int_cmp(int a, int b)
+{
+    return (a-b);
+}
+
+sll_node* sll_new_node(t_elem element) 
+{
+    sll_node* new_node = (sll_node*)malloc(sizeof(sll_node));
+    if (new_node != NULL) 
+    {
+        new_node->data = element;
+        new_node->next = NULL;
+    }
+    return new_node;
+}
+
+int sll_add_node(sll_node** head, sll_node* new_node)
+{
+    if (new_node == NULL) return 0;
+    while (*head != NULL) 
+    {
+        head = &(*head)->next;
+    }
+    *head = new_node;
+    return 1;
+}
+
+t_elem sll_node_free(sll_node** a_node)
+{
+    sll_node* aux = *a_node;
+    t_elem data = 0;
+    if (aux != NULL)
+    {
+        data = (aux)->data;
+        *a_node = aux->next;
+        free(aux);
+    }    
+    return data;
+}
+
+sll_node** sll_node_find(sll_node** a_node, t_elem e, int cmp(t_elem, t_elem)) 
+{
+    while (*a_node != NULL && cmp((*a_node)->data, e) != 0) 
+    {
+        a_node = &(*a_node)->next;
+    }
+    return a_node;
+}
+
+
+sll_node* sll_remove_element(sll_node** a_list, t_elem e, int cmp(t_elem, t_elem))
+{
+    sll_node** a_node = sll_node_find(a_list, e, cmp);
+    sll_node* aux = *a_node;
+    if (aux != NULL)
+    {
+        *a_node = aux->next;
+        aux->next = NULL;
+    }
+    return aux;
+}
+
+void sll_print(sll_node* a_node, void pnt(t_elem))
+{
+    while (a_node != NULL)
+    {
+        pnt(a_node->data);
+        a_node = a_node->next;
+    }
+}
+
+int sll_remove_all(sll_node** head, t_elem element, int cmp(t_elem, t_elem)) 
+{
+    int result = 0;
+    while (sll_remove_element(head, element, cmp) != NULL) 
+    {
+        result += 1;
+    }
+    return result;
+}
+
+void sll_remove_duplicated_elements(sll_node** a_node, int cmp(t_elem, t_elem))
+{
+    while (*a_node != NULL)
+    {
+        sll_remove_all(&(*a_node)->next, (*a_node)->data, cmp);
+        a_node = &(*a_node)->next;
+    }
+}
+
+int main()
+{
+    sll_node* head = NULL;
+    
+    sll_add_node(&head, sll_new_node(1));
+    sll_add_node(&head, sll_new_node(3));
+    sll_add_node(&head, sll_new_node(4));
+    sll_add_node(&head, sll_new_node(8));
+    sll_add_node(&head, sll_new_node(3));
+    sll_add_node(&head, sll_new_node(4));
+
+    //Eliminar todos los elementos repetidos de una lista dinámica simplemente enlazada (SSL).
+    printf("Lista antes de eliminar los elementos repetidos:\n");
+    sll_print(head, print_int);
+    printf("\n");
+    sll_remove_duplicated_elements(&head, int_cmp);
+    printf("Lista despues de eliminar los elementos repetidos:\n");
+    sll_print(head, print_int);
+    printf("\n");
+}
